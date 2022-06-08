8645c9dc121d33b6bac2e4e8a4ad88df56e3230d
GamalSerhanOporto <gamal.serhan@ucb.edu.bo>
2022-06-07 21:11:45 -0400

    css in register.

--- features/step_definitions/registerSteps.rb
+++ features/step_definitions/registerSteps.rb
@@ -40,6 +40,6 @@ And(/^the submit button should be disabled$/) do
 end
 
   When(/^I press the submit button$/) do
-    xpath = '/html/body/div/div[2]/div[1]/div/div/div[2]/div[3]/form/div[7]/button'
-    find(:xpath, xpath).click 
+    css = '#root > div:nth-child(2) > div:nth-child(2) > div > div > div.MuiGrid-root > div.MuiPaper-root.MuiCard-root.jss13.MuiPaper-elevation1.MuiPaper-rounded > form > div.jss15 > button > span.MuiButton-label'
+    find(:css, css).click 
   end
